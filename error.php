1ac096de8becde5b12415e3a1e258f076c1d4d34
H-axame <68094943+H-axame@users.noreply.github.com>
2023-06-13 13:52:17 +0800

    Add files via upload

---
+++ error.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Error</title>
+  <style type="text/css">
+    body {
+      background-color: #f0f0f0;
+      font-family: Arial, sans-serif;
+      text-align: center;
+    }
+
+    .container {
+      margin-top: 100px;
+    }
+
+    h1 {
+      font-size: 36px;
+      font-weight: bold;
+      color: #333333;
+    }
+
+    p {
+      font-size: 14px;
+      color: #666666;
+      margin-top: 10px;
+    }
+
+    a {
+      color: #333333;
+      text-decoration: none;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>You must log in</h1>
+    <p>
+      <a href="index.php">Click here to go back to the main page</a>
+    </p>
+  </div>
+</body>
+</html>
+
